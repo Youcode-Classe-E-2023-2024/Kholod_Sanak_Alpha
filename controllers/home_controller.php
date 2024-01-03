779322bd702bc985e3cd758680792c5f65348ec6
kholoud001 <kholoud.sanak@gmail.com>
2024-01-03 15:28:51 +0100

    test

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -9,3 +9,12 @@
 //if (isset($_POST['logout'])) {
 //    echo 'Is logout';
 //}
+
+
+if (isset($_POST["products"])) {
+    header("Access-Control-Allow-Origin: *");
+    $url = "https://jsonplaceholder.typicode.com/posts";
+    $data = file_get_contents($url);
+    echo $data;
+    exit;
+}
---
+++ views/home_view.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<script>
+
+    function getProducts() {
+        $.ajax({
+            type: "POST",
+            url: "controllers/home_controller.php",
+            data: {products: true},
+            success: (data) => {
+                console.log(data);
+            }
+        });
+    }
+
+    getProducts();
+</script>
