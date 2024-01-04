47a2b7c90a31e0ad53c268686e32f7023653ce33
kholoud001 <kholoud.sanak@gmail.com>
2024-01-04 17:10:55 +0100

    home controller update

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -10,11 +10,3 @@
 //    echo 'Is logout';
 //}
 
-
-if (isset($_POST["products"])) {
-    header("Access-Control-Allow-Origin: *");
-    $url = "https://jsonplaceholder.typicode.com/posts";
-    $data = file_get_contents($url);
-    echo $data;
-    exit;
-}
