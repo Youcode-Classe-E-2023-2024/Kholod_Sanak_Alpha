11f7e09809018ded3fb8cef3bbbf9fab0a609f28
kholoud001 <kholoud.sanak@gmail.com>
2024-01-04 14:17:49 +0100

    products view

--- controllers/products_controller.php
+++ controllers/products_controller.php
@@ -1,5 +1,14 @@
 <?php
 
+
+
+if (isset($_POST["products"])) {
+    header("Access-Control-Allow-Origin: *");
+    $url = "https://jsonplaceholder.typicode.com/posts";
+    $data = file_get_contents($url);
+    echo $data;
+    exit;
+}
 if (empty($_SESSION['user_id'])) {
     header('location: index.php?page=authentif');
 }
