7c63da36010224d9f587a77f89e3d68ff8134efe
kholoud001 <kholoud.sanak@gmail.com>
2024-01-04 14:19:26 +0100

    users

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -1,5 +1,14 @@
 <?php
 
+
+if (isset($_POST["users"])) {
+    header("Access-Control-Allow-Origin: *");
+    $url = "https://jsonplaceholder.typicode.com/users";
+    $data = file_get_contents($url);
+    echo $data;
+    exit;
+}
+
 if (empty($_SESSION['user_id'])) {
     header('location: index.php?page=authentif');
 }
