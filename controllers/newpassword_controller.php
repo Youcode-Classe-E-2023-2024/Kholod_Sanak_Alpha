f1eee5aa051b6c3e0d1176e34277c4e3c6797594
kholoud001 <kholoud.sanak@gmail.com>
2024-01-04 10:22:43 +0100

    pwd recovery

--- controllers/newpassword_controller.php
+++ controllers/newpassword_controller.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 //include("./../db.inc.php");
@@ -20,7 +22,7 @@ if (isset($_POST["newPassword"])) {
     }
 
     $sql = "SELECT * FROM passwordRecovery WHERE pwd_reset_selector= ?";
-    $stmt = mysqli_stmt_init($conn);
+    $stmt = mysqli_stmt_init($db);
     mysqli_stmt_prepare($stmt, $sql);
     mysqli_stmt_bind_param($stmt, "s", $selector);
     mysqli_stmt_execute($stmt);
@@ -37,24 +39,23 @@ if (isset($_POST["newPassword"])) {
     $hashedValidator = $row["pwd_reset_token"];
     $expiresDate = $row["pwd_reset_expires"];
     if (!password_verify($validator, $hashedValidator) && $actualDate <= $expiresDate) {
-        header("Location: ../index.php?page=newpassword");
+        header("Location: index.php?page=newpassword");
     } else {
         $user_email = $row["pwd_reset_email"];
         $newHashedPsswd = password_hash($password, PASSWORD_DEFAULT);
         $sql = "UPDATE user SET password=? WHERE email='$user_email'";
-        $stmt = mysqli_stmt_init($conn);
+        $stmt = mysqli_stmt_init($db);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             echo "There is an error";
         } else {
             mysqli_stmt_bind_param($stmt, "s", $newHashedPsswd);
             mysqli_stmt_execute($stmt);
             $sql = "DELETE FROM passwordRecovery WHERE pwd_reset_email='$user_email'";
-            $stmt = mysqli_stmt_init($conn);
+            $stmt = mysqli_stmt_init($db);
             mysqli_stmt_prepare($stmt, $sql);
             mysqli_stmt_execute($stmt);
         }
     }
     mysqli_stmt_close($stmt);
-    header("Location: ../index.php?page=authentif");
-} else
-    header("Location: ../index.php?page=dashboard");
+    header("Location: index.php?page=authentif");
+}
