1da5160a65a4643ba424e61b205be353a423fefa
kholoud001 <kholoud.sanak@gmail.com>
2024-01-04 14:19:34 +0100

    users

--- views/users_view.php
+++ views/users_view.php
@@ -298,7 +298,7 @@
                                                 <th
                                                     class="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                                     <div class="flex cursor-pointer">
-                                                        <span class="mr-2">Number ID</span>
+                                                        <span class="mr-2">Phone</span>
                                                     </div>
                                                 </th>
                                                 <th
@@ -315,63 +315,63 @@
                                                 </th>
                                             </tr>
                                             </thead>
-                                            <tbody class="bg-white divide-y divide-gray-200">
-                                            <tr>
-                                                <td
-                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                    <p>Username</p>
-                                                    <p class="text-xs text-gray-400">NAME
-                                                    </p>
-                                                </td>
-                                                <td
-                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                    <p>77</p>
-                                                </td>
-                                                <td
-                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                    <div class="flex text-grey-500">
-                                                        <!-- <svg xmlns="http://www.w3.org/2000/svg"
-                                                            class="w-5 h-5 mr-1" fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            stroke="currentColor">
-                                                            <path stroke-linecap="round"
-                                                                stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                        </svg> -->
-                                                        <p>EMAIL</p>
-                                                    </div>
-                                                </td>
-                                                <td
-                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                    <div class="flex space-x-4">
-                                                        <a href="#" class="text-blue-500 hover:text-blue-600">
-                                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                                 class="w-5 h-5 mr-1"
-                                                                 fill="none" viewBox="0 0 24 24"
-                                                                 stroke="currentColor">
-                                                                <path stroke-linecap="round"
-                                                                      stroke-linejoin="round"
-                                                                      stroke-width="2"
-                                                                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                                            </svg>
-                                                            <p>Edit</p>
-                                                        </a>
-                                                        <a href="#" class="text-red-500 hover:text-red-600">
-                                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                                 class="w-5 h-5 mr-1 ml-3"
-                                                                 fill="none" viewBox="0 0 24 24"
-                                                                 stroke="currentColor">
-                                                                <path stroke-linecap="round"
-                                                                      stroke-linejoin="round"
-                                                                      stroke-width="2"
-                                                                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                            </svg>
-                                                            <p>Delete</p>
-                                                        </a>
-                                                    </div>
-                                                </td>
-                                            </tr>
+                                            <tbody id="user-table" class="bg-white divide-y divide-gray-200">
+<!--                                            <tr>-->
+<!--                                                <td-->
+<!--                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">-->
+<!--                                                    <p>Username</p>-->
+<!--                                                    <p class="text-xs text-gray-400">NAME-->
+<!--                                                    </p>-->
+<!--                                                </td>-->
+<!--                                                <td-->
+<!--                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">-->
+<!--                                                    <p>77</p>-->
+<!--                                                </td>-->
+<!--                                                <td-->
+<!--                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">-->
+<!--                                                    <div class="flex text-grey-500">-->
+<!--                                                        <svg xmlns="http://www.w3.org/2000/svg"-->
+<!--                                                            class="w-5 h-5 mr-1" fill="none"-->
+<!--                                                            viewBox="0 0 24 24"-->
+<!--                                                            stroke="currentColor">-->
+<!--                                                            <path stroke-linecap="round"-->
+<!--                                                                stroke-linejoin="round"-->
+<!--                                                                stroke-width="2"-->
+<!--                                                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />-->
+<!--                                                        </svg> -->
+<!--                                                        <p>EMAIL</p>-->
+<!--                                                    </div>-->
+<!--                                                </td>-->
+<!--                                                <td-->
+<!--                                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5">-->
+<!--                                                    <div class="flex space-x-4">-->
+<!--                                                        <a href="#" class="text-blue-500 hover:text-blue-600">-->
+<!--                                                            <svg xmlns="http://www.w3.org/2000/svg"-->
+<!--                                                                 class="w-5 h-5 mr-1"-->
+<!--                                                                 fill="none" viewBox="0 0 24 24"-->
+<!--                                                                 stroke="currentColor">-->
+<!--                                                                <path stroke-linecap="round"-->
+<!--                                                                      stroke-linejoin="round"-->
+<!--                                                                      stroke-width="2"-->
+<!--                                                                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />-->
+<!--                                                            </svg>-->
+<!--                                                            <p>Edit</p>-->
+<!--                                                        </a>-->
+<!--                                                        <a href="#" class="text-red-500 hover:text-red-600">-->
+<!--                                                            <svg xmlns="http://www.w3.org/2000/svg"-->
+<!--                                                                 class="w-5 h-5 mr-1 ml-3"-->
+<!--                                                                 fill="none" viewBox="0 0 24 24"-->
+<!--                                                                 stroke="currentColor">-->
+<!--                                                                <path stroke-linecap="round"-->
+<!--                                                                      stroke-linejoin="round"-->
+<!--                                                                      stroke-width="2"-->
+<!--                                                                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />-->
+<!--                                                            </svg>-->
+<!--                                                            <p>Delete</p>-->
+<!--                                                        </a>-->
+<!--                                                    </div>-->
+<!--                                                </td>-->
+<!--                                            </tr>-->
                                             </tbody>
                                         </table>
                                     </div>
@@ -388,6 +388,62 @@
 </main>
 </div>
 </div>
+
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<script>
+    function getProducts() {
+        $.ajax({
+            type: "POST",
+            url: "controllers/users_controller.php",
+            data: { users: true },
+            success: function (data) {
+                console.log(data);
+                let users = JSON.parse(data);
+
+                // Clear existing table rows
+                $('#user-table').empty();
+
+                users.forEach(user => {
+                    let row = `
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                <p>${user.username}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                <p>${user.phone}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                    <p>${user.email}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                <div class="flex space-x-4">
+                                    <a href="#" class="text-blue-500 hover:text-blue-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                        </svg>
+                                        <p>Edit</p>
+                                    </a>
+                                    <a href="#" class="text-red-500 hover:text-red-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                        <p>Delete</p>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    `;
+                    $('#user-table').append(row);
+                });
+            }
+        });
+    }
+
+    getProducts();
+</script>
+
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
     function data() {
