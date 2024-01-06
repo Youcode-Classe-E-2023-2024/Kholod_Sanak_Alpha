7f1ffef75870ef93dab28f180f7598835385df46
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 15:28:05 +0100

    notification

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -1,12 +1,32 @@
 <?php
-//
-//echo '<h3>- Home Controller -</h3>';
-//
-//if (isset($_POST['login'])) {
-//    echo 'Is login';
-//}
-//
-//if (isset($_POST['logout'])) {
-//    echo 'Is logout';
-//}
 
+
+if (isset($_POST["request"]) && $_POST["request"] === "addNotification") {
+    $notification = new Notification;
+    $notificationDetail = $_POST["notification"];
+    $user = $_SESSION["user_id"];
+    $notification->addNotification($user, $notificationDetail);
+    exit;
+}
+
+if (isset($_POST["request"]) && $_POST["request"] === "displayNotification") {
+    $notification = new Notification;
+    $notificationData = $notification->showNotifications($_SESSION["user_id"]);
+
+    echo json_encode($notificationData);
+    exit;
+}
+
+if (isset($_POST["request"]) && $_POST["request"] === "notificationSeen") {
+    $notification = new Notification;
+    $notificationId = $_POST["notificationId"];
+    $notification->notificationSeen($notificationId);
+
+    echo "nadiii";
+    exit;
+}
+
+
+if (empty($_SESSION['user_id'])) {
+    header('location: index.php?page=authentif');
+}
