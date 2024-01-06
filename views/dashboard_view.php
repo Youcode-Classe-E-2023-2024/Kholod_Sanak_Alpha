6230e9c84644a4d974cc6c9d7ef72acef2b9001b
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 09:20:50 +0100

    dashboard update

--- views/dashboard_view.php
+++ views/dashboard_view.php
@@ -297,14 +297,15 @@
                                             </svg>
                                             <div
                                                 class="bg-green-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
-                                                <span class="flex items-center">30%</span>
+                                                <span class="flex items-center">Qte</span>
                                             </div>
                                         </div>
-                                        <div class="ml-2 w-full flex-1">
-                                            <div>
-                                                <div class="mt-3 text-3xl font-bold leading-8">4.510</div>
 
-                                                <div class="mt-1 text-base text-gray-600">Item Sales</div>
+                                        <!--product number -->
+                                        <div class="ml-2 w-full flex-1 product_nbr">
+                                            <div>
+                                                <div class="mt-3 text-3xl font-bold leading-8" id="posts_number">Loading...</div>
+                                                <div class="mt-1 text-base text-gray-600">Products</div>
                                             </div>
                                         </div>
                                     </div>
@@ -321,69 +322,18 @@
                                             </svg>
                                             <div
                                                 class="bg-red-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
-                                                <span class="flex items-center">30%</span>
+                                                <span class="flex items-center">Nbr</span>
                                             </div>
                                         </div>
-                                        <div class="ml-2 w-full flex-1">
+                                        <div class="ml-2 w-full flex-1 user_nbr">
                                             <div>
-                                                <div class="mt-3 text-3xl font-bold leading-8">4.510</div>
-
-                                                <div class="mt-1 text-base text-gray-600">Item Sales</div>
+                                                <div class="mt-3 text-3xl font-bold leading-8" id="users_number">Loading...</div>
+                                                <div class="mt-1 text-base text-gray-600">Users</div>
                                             </div>
                                         </div>
                                     </div>
                                 </a>
-                                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                                   href="#">
-                                    <div class="p-5">
-                                        <div class="flex justify-between">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-pink-600"
-                                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      stroke-width="2"
-                                                      d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      stroke-width="2"
-                                                      d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
-                                            </svg>
-                                            <div
-                                                class="bg-yellow-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
-                                                <span class="flex items-center">30%</span>
-                                            </div>
-                                        </div>
-                                        <div class="ml-2 w-full flex-1">
-                                            <div>
-                                                <div class="mt-3 text-3xl font-bold leading-8">4.510</div>
 
-                                                <div class="mt-1 text-base text-gray-600">Item Sales</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                                   href="#">
-                                    <div class="p-5">
-                                        <div class="flex justify-between">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-green-400"
-                                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      stroke-width="2"
-                                                      d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
-                                            </svg>
-                                            <div
-                                                class="bg-blue-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
-                                                <span class="flex items-center">30%</span>
-                                            </div>
-                                        </div>
-                                        <div class="ml-2 w-full flex-1">
-                                            <div>
-                                                <div class="mt-3 text-3xl font-bold leading-8">4.510</div>
-
-                                                <div class="mt-1 text-base text-gray-600">Item Sales</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
                             </div>
                         </div>
                         <div class="col-span-12 mt-5">
@@ -394,6 +344,36 @@
                         </div>
 
                         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+                        <script>
+                            // Fetch posts and update the content of the div
+                            const postsNumberElement = document.getElementById('posts_number');
+
+                            fetch('https://jsonplaceholder.typicode.com/posts')
+                                .then(res => res.json())
+                                .then(data => {
+                                    // Update the content with the length of posts
+                                    postsNumberElement.textContent = data.length;
+                                })
+                                .catch(error => {
+                                    console.error('Error fetching posts:', error);
+                                    postsNumberElement.textContent = 'Error loading posts';
+                                });
+
+                            // Fetch posts and update the content of the div
+                            const usersNumberElement = document.getElementById('users_number');
+
+                            fetch('https://jsonplaceholder.typicode.com/users')
+                                .then(res => res.json())
+                                .then(data => {
+                                    // Update the content with the length of posts
+                                    usersNumberElement.textContent = data.length;
+                                })
+                                .catch(error => {
+                                    console.error('Error fetching posts:', error);
+                                    usersNumberElement.textContent = 'Error loading posts';
+                                });
+                        </script>
                         <script>
                             function data() {
 
