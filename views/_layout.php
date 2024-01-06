a9c0166f86b5419139eeeecc7c30a4e77c8f8a5f
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 12:12:29 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -14,6 +14,7 @@
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
+
 </head>
 <body >
 
