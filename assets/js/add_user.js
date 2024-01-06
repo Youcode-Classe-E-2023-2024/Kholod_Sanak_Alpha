bf00d16959f12173bed74b0047b159e18a7b3091
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 08:22:57 +0100

    add user update

--- assets/js/add_user.js
+++ assets/js/add_user.js
@@ -1,26 +1,50 @@
-$(document).ready(function () {
-    $('#addUserForm').submit(function (e) {
-        e.preventDefault();
+function addUserFormSubmit() {
+    //e.preventDefault();
 
-        // Collect form data
-        let formData = {
-            username: $('#username').val(),
-            phone: $('#phone').val(),
-            email: $('#email').val(),
-        };
+    let formData = {
+        username: $('#username').val(),
+        phone: $('#phone').val(),
+        email: $('#email').val(),
+    };
+    console.log(formData);
 
-        // Send POST request to the server
-        $.ajax({
-            type: "POST",
-            url: "https://jsonplaceholder.typicode.com/users",
-            data: { userData: formData },
-            success: function (data,status) {
-                if (status=== "success") {
-                    alert("User has been added successfully!");
-                } else {
-                    alert("Failed to add user. Please try again.");
-                }
+    // Send POST request to the server
+    $.ajax({
+        type: "POST",
+        url: "https://jsonplaceholder.typicode.com/users",
+        data: formData, // Removed unnecessary object wrapping
+        success: function (data, status) {
+            console.log(status);
+            if (status === "success") {
+                alert("User has been added successfully!");
+            } else {
+                alert("Failed to add user. Please try again.");
             }
-        });
+        }
     });
+}
+
+function addMultipleUsers(userArray) {
+    userArray.forEach((user) => {
+        addUserFormSubmit();
+    });
+}
+
+$(document).ready(function () {
+    $('#save').click(addUserFormSubmit);
+});
+
+$('#save').click(function () {
+    //e.preventDefault();
+    var userForms = $("#addUserForm"); 
+    var formData = [];
+
+    userForms.each(function (index, form) {
+        var username = $(form).find('[id="username"]').val();
+        var email = $(form).find('[id="email"]').val();
+        var phone = $(form).find('[id="phone"]').val();
+
+        formData.push({ username: username, email: email, phone: phone }); // Fixed the property names
+    });
+    addMultipleUsers(formData);
 });
