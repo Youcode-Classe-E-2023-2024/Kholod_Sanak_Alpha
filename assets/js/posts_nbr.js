1b9bd2d8423e3640d10b6e8f87753c78554b2bfa
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 12:12:05 +0100

    posts nbr

---
+++ assets/js/posts_nbr.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Fetch posts and update the content of the div
+    const postsNumberElement = $('#posts_number');
+
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        dataType: 'json',
+        success: function (data) {
+            //console.log(data)
+            postsNumberElement.text(data.length);
+        },
+        error: function (error) {
+            console.error('Error fetching posts:', error);
+            postsNumberElement.text('Error loading posts');
+        }
+    });
+});
