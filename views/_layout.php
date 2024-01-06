01a0931cd7e2eadec14a20aa687cba21ee370a9b
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 15:28:21 +0100

    layout update

--- views/_layout.php
+++ views/_layout.php
@@ -12,7 +12,10 @@
 
     <!--Favicon-->
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.7.1.js"
+            integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+            crossorigin="anonymous"></script>
 
 
 </head>
