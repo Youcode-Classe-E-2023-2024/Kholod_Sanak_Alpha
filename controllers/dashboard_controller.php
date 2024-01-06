a86171491a4dfc425b0fa011b3a621e57139a268
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 15:27:59 +0100

    notification

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -1,4 +1,36 @@
 <?php
+
+
+if (isset($_POST["request"]) && $_POST["request"] === "addNotification") {
+    $notification = new Notification;
+    $notificationDetail = $_POST["notification"];
+    $user = $_SESSION["user_id"];
+    $notification->addNotification($user, $notificationDetail);
+    exit;
+}
+
+if (isset($_POST["request"]) && $_POST["request"] === "displayNotification") {
+    $notification = new Notification;
+    $notificationData = $notification->showNotifications($_SESSION["user_id"]);
+
+    echo json_encode($notificationData);
+    exit;
+}
+
+if (isset($_POST["request"]) && $_POST["request"] === "notificationSeen") {
+    $notification = new Notification;
+    $notificationId = $_POST["notificationId"];
+    $notification->notificationSeen($notificationId);
+
+    echo "nadiii";
+    exit;
+}
+
+
 if (empty($_SESSION['user_id'])) {
     header('location: index.php?page=authentif');
+}
+
+if (empty($_SESSION['user_id'])) {
+header('location: index.php?page=authentif');
 }
