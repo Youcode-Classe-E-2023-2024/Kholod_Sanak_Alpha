78b3cb61a6439e5429bbad730e96e078575e8f5c
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 15:29:59 +0100

    notification

--- assets/js/add_product.js
+++ assets/js/add_product.js
@@ -15,6 +15,8 @@ function addProductFormSubmit(e) {
         success: function (data, status) {
             //console.log(status);
             if (status === "success") {
+                addToNotification("Product Added Successfully: Your Newest Addition is Now Live!");
+
                 alert("Product has been added successfully!");
             } else {
                 alert("Failed to add product. Please try again.");
@@ -22,8 +24,41 @@ function addProductFormSubmit(e) {
         }
     });
 }
+displayNotification();
+
+
+function addToNotification(notification) {
+    $.ajax({
+        type: "POST",
+        url: "index.php?page=dashboard",
+        data: {
+            request: "addNotification",
+            notification,
+        },
+        success: (data) => {
+            console.log(data);
+        }
+    })
+}
+function displayNotification() {
+    $.ajax({
+        type: 'post',
+        url: 'index.php?page=dashboard',
+        data: {
+            request: "displayNotification"
+        },
+        success: (data) => {
+            console.log(data);
+        }
+    })
+}
+
+
+
 
 
 $(document).ready(function () {
     $('#save').click(addProductFormSubmit);
+
 });
+
