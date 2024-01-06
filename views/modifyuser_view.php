390dc813c07ebbb60fd8c1ae2c7a78cddc1d4ac2
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 12:11:38 +0100

    modify user update

--- views/modifyuser_view.php
+++ views/modifyuser_view.php
@@ -11,7 +11,7 @@
                     </div>
                     <!-- Username -->
                     <div class="lg:col-span-2">
-                        <form id="modifyProductForm">
+                        <form id="modifyUserForm">
                             <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
                                 <div class="md:col-span-3">
                                     <label for="username">Username</label>
