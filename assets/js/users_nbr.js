499ad61e62e5f0490bcee3630e07b48b94ee2bae
kholoud001 <kholoud.sanak@gmail.com>
2024-01-06 12:11:56 +0100

    nbt users

---
+++ assets/js/users_nbr.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Fetch posts and update the content of the div
+    const usersNumberElement = $('#users_number');
+
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/users',
+        dataType: 'json',
+        success: function (data) {
+            //console.log(data)
+            usersNumberElement.text(data.length);
+        },
+        error: function (error) {
+            console.error('Error fetching posts:', error);
+            usersNumberElement.text('Error loading users');
+        }
+    });
+});
